371a20a3c018cebca88817b14b5b109ebb1b0948
Oliver Hader <oliver@typo3.org>
2020-01-09 20:58:22 +0100

    [TASK] Add tests for Book model

--- tests/Domain/BookTest.php
+++ tests/Domain/BookTest.php
@@ -15,4 +15,34 @@ class BookTest extends TestCase
         $subject = new Book();
         self::assertInstanceOf(Book::class, $subject);
     }
+
+    public function bookPropertyIsAssignedDataProvider(): array
+    {
+        return [
+            ['id', rand(1, 1000)],
+            ['isbn', uniqid('isbn')],
+            ['title', uniqid('title')],
+            ['blurb', uniqid('blurb')],
+            ['price', rand(1, 1000) / 100],
+        ];
+    }
+
+    /**
+     * @param string $propertyName
+     * @param $value
+     *
+     * @test
+     * @dataProvider bookPropertyIsAssignedDataProvider
+     */
+    public function bookPropertyIsAssigned(string $propertyName, $value)
+    {
+        $setMethod = 'set' . ucfirst($propertyName);
+        $getMethod = 'get' . ucfirst($propertyName);
+
+        $subject = new Book();
+        $subject->{$setMethod}($value);
+        $result = $subject->{$getMethod}();
+
+        self::assertSame($value, $result);
+    }
 }
